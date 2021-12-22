6ba8a774b4d954fea413d6878165b1665d46438d
Sara McGhee <sarajmcghee@gmail.com>
2021-12-21 20:45:06 -0500

    navbar completeish

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,6 @@
 $(function () {
-    $(".carousel").carousel({ interval: 2000 });
-    $("#carouselButton").click(function () {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
-    });
-    $("#reserveButton").click(function () {
-        $("#reserveModal").modal(focus);
-    });
-    $("#loginButton").click(function () {
-        $("#loginModal").modal(focus);
-    });
+    $(".carousel").carousel({ interval: 5000 });
+
 
 });
 
